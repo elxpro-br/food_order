f3978b86db772c4f94062744b269898b3d533a37
Gustavo Oliveira <g.92oliveira@gmail.com>
2022-02-01 17:54:08 -0300

    pipe ci

--- mix.exs
+++ mix.exs
@@ -48,7 +48,11 @@ defmodule FoodOrder.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:sobelow, "~> 0.8", only: :dev},
+      {:dialyxir, "~> 0.5.0", only: [:dev, :test], runtime: false}
     ]
   end
 
