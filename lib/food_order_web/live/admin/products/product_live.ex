b9d0e6e709f09c8281b4c2028325c12cbcac5a43
Gustavo Oliveira <g.92oliveira@gmail.com>
2022-02-21 21:12:52 -0300

    edit product

--- lib/food_order_web/live/admin/products/product_live.ex
+++ lib/food_order_web/live/admin/products/product_live.ex
@@ -1,10 +1,36 @@
 defmodule FoodOrderWeb.Admin.ProductLive do
   use FoodOrderWeb, :live_view
   alias FoodOrder.Products
+  alias FoodOrder.Products.Product
   alias FoodOrderWeb.Admin.Products.Form
 
   def mount(_p, _s, socket) do
     products = Products.list_products()
     {:ok, socket |> assign(products: products)}
+  end
+
+  def handle_params(params, _url, socket) do
+    live_action = socket.assigns.live_action
+    {:noreply, apply_action(socket, live_action, params)}
+  end
+
+  defp apply_action(socket, :new, _params) do
+    socket
+    |> assign(:page_title, "Create new Product")
+    |> assign(:product, %Product{})
+  end
+
+  defp apply_action(socket, :edit, %{"id" => id}) do
+    product = Products.get!(id)
+
+    socket
+    |> assign(:page_title, "Edit Product")
+    |> assign(:product, product)
+  end
+
+  defp apply_action(socket, :index, _params) do
+    socket
+    |> assign(:page_title, "List Products")
+    |> assign(:product, nil)
   end
 end
