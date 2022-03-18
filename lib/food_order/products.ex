a27d5b78586a5ad61af975d895f165691c750407
Gustavo Oliveira <g.92oliveira@gmail.com>
2022-03-18 18:29:30 -0300

    fix tests

--- lib/food_order/products.ex
+++ lib/food_order/products.ex
@@ -27,8 +27,15 @@ defmodule FoodOrder.Products do
   def change_product(product, params \\ %{}), do: Product.changeset(product, params)
 
   def get_image(product) do
-    url = ProductImage.url({product.product_url, product})
-    [_, url] = String.split(url, "/priv/static")
+    {product.product_url, product}
+    |> ProductImage.url()
+    |> get_image_url()
+  end
+
+  defp get_image_url(nil), do: ""
+
+  defp get_image_url(url) do
+    [_ | url] = String.split(url, "/priv/static")
     url
   end
 end
