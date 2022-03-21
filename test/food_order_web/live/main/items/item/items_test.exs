8167a3aaa936e251d8d6475f5f38c9c776614c99
Gustavo Oliveira <g.92oliveira@gmail.com>
2022-03-21 18:55:28 -0300

    create tests

--- test/food_order_web/live/main/items/item/items_test.exs
+++ test/food_order_web/live/main/items/item/items_test.exs
@@ -1,15 +1,27 @@
 defmodule FoodOrderWeb.Main.Items.ItemTest do
   use FoodOrderWeb.ConnCase
+  import FoodOrder.Factory
   import Phoenix.LiveViewTest
 
   test "load item", %{conn: conn} do
+    product = insert(:product)
     {:ok, view, _html} = live(conn, Routes.main_path(conn, :index))
-    assert has_element?(view, "#item-1")
-    assert has_element?(view, "[data-role=product-img][data-id=item-1]")
+    assert has_element?(view, "#item-#{product.id}")
+    assert has_element?(view, "[data-role=product-img][data-id=item-#{product.id}]")
     assert has_element?(view, "[data-role=product-description]")
-    assert has_element?(view, "[data-role=product-name][data-id=item-1]", "Produto com Nome")
-    assert has_element?(view, "[data-role=product-price][data-id=item-1]", "$ 10")
-    assert has_element?(view, "[data-role=product-price][data-id=item-1]", "$ 10")
-    assert has_element?(view, "[data-role=product-add][data-id=item-1]")
+
+    assert has_element?(
+             view,
+             "[data-role=product-name][data-id=item-#{product.id}]",
+             product.name
+           )
+
+    assert has_element?(
+             view,
+             "[data-role=product-price][data-id=item-#{product.id}]",
+             Money.to_string(product.price)
+           )
+
+    assert has_element?(view, "[data-role=product-add][data-id=item-#{product.id}]")
   end
 end
