0a25f47d9e0f67edecd676698093399aad6834d8
Gustavo Oliveira <g.92oliveira@gmail.com>
2022-03-22 18:25:07 -0300

    test everything

--- lib/food_order_web/live/admin/products/product_live.ex
+++ lib/food_order_web/live/admin/products/product_live.ex
@@ -15,12 +15,12 @@ defmodule FoodOrderWeb.Admin.ProductLive do
   def handle_params(params, _url, socket) do
     live_action = socket.assigns.live_action
     products = Products.list_products()
+    assigns = [products: products, name: "", loading: false]
 
     socket =
       socket
       |> apply_action(live_action, params)
-      |> assign(products: products)
-      |> assign(name: "")
+      |> assign(assigns)
 
     {:noreply, socket}
   end
@@ -35,6 +35,11 @@ defmodule FoodOrderWeb.Admin.ProductLive do
   def handle_event("filter-by-name", %{"name" => name}, socket) do
     socket = apply_filters(socket, name)
     {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info({:list_products, name}, socket) do
+    {:noreply, perfom_filter(socket, name)}
   end
 
   defp apply_action(socket, :new, _params) do
@@ -58,7 +63,27 @@ defmodule FoodOrderWeb.Admin.ProductLive do
   end
 
   defp apply_filters(socket, name) do
-    products = Products.list_products(name)
-    assign(socket, products: products, name: name)
+    assigns = [products: [], name: name, loading: true]
+    send(self(), {:list_products, name})
+    assign(socket, assigns)
+  end
+
+  defp perfom_filter(socket, name) do
+    name
+    |> Products.list_products()
+    |> return_filter_response(socket, name)
+  end
+
+  defp return_filter_response([], socket, name) do
+    assigns = [loading: false, products: []]
+
+    socket
+    |> put_flash(:info, "There is no product with \"#{name}\"")
+    |> assign(assigns)
+  end
+
+  defp return_filter_response(products, socket, _name) do
+    assigns = [loading: false, products: products]
+    assign(socket, assigns)
   end
 end
