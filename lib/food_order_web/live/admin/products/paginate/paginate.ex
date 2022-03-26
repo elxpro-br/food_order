b59e7d2682480d47976f7c154456399a010c6124
Gustavo Oliveira <g.92oliveira@gmail.com>
2022-03-26 08:14:06 -0300

    select per page

--- lib/food_order_web/live/admin/products/paginate/paginate.ex
+++ lib/food_order_web/live/admin/products/paginate/paginate.ex
@@ -15,4 +15,17 @@ defmodule FoodOrderWeb.Admin.Product.Paginate do
               ), "data-role": "paginate", "data-id": data_id) %>
     """
   end
+
+  def handle_event("update-select-perpage", %{"per-page-select" => per_page_value}, socket) do
+    to =
+      Routes.admin_product_path(socket, :index,
+        page: socket.assigns.options.page,
+        per_page: per_page_value,
+        sort_by: socket.assigns.options.sort_by,
+        sort_order: socket.assigns.options.sort_order,
+        name: socket.assigns.name
+      )
+
+    {:noreply, push_patch(socket, to: to)}
+  end
 end
