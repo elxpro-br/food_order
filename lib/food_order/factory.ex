b5ac7dfe3ac366714eaa4c3b143c329d4777dd26
Gustavo Oliveira <g.92oliveira@gmail.com>
2022-04-19 19:02:32 -0300

    test hooks

--- lib/food_order/factory.ex
+++ lib/food_order/factory.ex
@@ -5,7 +5,7 @@ defmodule FoodOrder.Factory do
   def product_factory do
     %Product{
       description: Faker.Food.description(),
-      name: Faker.Food.dish(),
+      name: Faker.Food.dish() <> (:rand.uniform(1_000) |> Integer.to_string()),
       price: :rand.uniform(10_000),
       size: "small"
     }
--- test/food_order_web/live/main/items/items_test.exs
+++ test/food_order_web/live/main/items/items_test.exs
@@ -1,10 +1,35 @@
 defmodule FoodOrderWeb.Main.ItemsTest do
   use FoodOrderWeb.ConnCase
+
+  import FoodOrder.Factory
   import Phoenix.LiveViewTest
 
   test "load items", %{conn: conn} do
     {:ok, view, _html} = live(conn, Routes.main_path(conn, :index))
     assert has_element?(view, "#items-component")
     assert has_element?(view, "[data-role=items-info][data-id=all-food]", "All Foods")
+  end
+
+  test "should load more elements", %{conn: conn} do
+    products = for _ <- 0..12, do: insert(:product)
+    {:ok, view, _html} = live(conn, Routes.main_path(conn, :index))
+
+    [products_page_1, products_page_2] = products |> Enum.chunk_every(8)
+
+    Enum.each(products_page_1, fn product ->
+      assert has_element?(view, "#item-#{product.id}")
+    end)
+
+    Enum.each(products_page_2, fn product ->
+      refute has_element?(view, "#item-#{product.id}")
+    end)
+
+    view
+    |> element("#products-loading")
+    |> render_hook("load-more", %{})
+
+    Enum.each(products_page_2, fn product ->
+      assert has_element?(view, "#item-#{product.id}")
+    end)
   end
 end
