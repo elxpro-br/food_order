18ea18ec0aeb01f1bf97aed286c9f4a4f6feb620
Gustavo <g.92oliveira@gmail.com>
2022-05-16 18:10:14 -0300

    add same item

--- test/food_order/carts/core/handle_carts_test.exs
+++ test/food_order/carts/core/handle_carts_test.exs
@@ -2,31 +2,42 @@ defmodule FoodOrder.Carts.Core.HandleCartsTest do
   use FoodOrder.DataCase
   import FoodOrder.Factory
 
-  alias FoodOrder.Carts.Core.HandleCarts
+  import FoodOrder.Carts.Core.HandleCarts
   alias FoodOrder.Carts.Data.Cart
 
   @start_cart %Cart{
     id: 444_444,
     items: [],
-    total_items: 0,
     total_price: %Money{amount: 0, currency: :BRL},
     total_qty: 0
   }
 
   describe "handle carts" do
     test "should create a new cart" do
-      assert @start_cart == HandleCarts.create_carts(444_444)
+      assert @start_cart == create_carts(444_444)
     end
 
     test "should add a new item in the cart" do
       product = insert(:product)
 
-      cart = HandleCarts.add_new_product(@start_cart, product)
+      cart = add_new_product(@start_cart, product)
 
       assert 1 == cart.total_qty
-      assert [product] == cart.items
+      assert [%{item: product, qty: 1}] == cart.items
       assert product.price == cart.total_price
-      assert 1 == cart.total_items
+    end
+
+    test "should add the same item twice" do
+      product = insert(:product)
+
+      cart =
+        @start_cart
+        |> add_new_product(product)
+        |> add_new_product(product)
+
+      assert 2 == cart.total_qty
+      assert Money.add(product.price, product.price) == cart.total_price
+      assert [%{item: product, qty: 2}] == cart.items
     end
   end
 end
