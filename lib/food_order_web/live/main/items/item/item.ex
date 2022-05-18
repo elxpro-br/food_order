2bf1551d1fce124c583c6579ebc21a38d327c0ba
Gustavo <g.92oliveira@gmail.com>
2022-05-18 10:04:12 -0300

    add item to cart

--- lib/food_order_web/live/main/items/item/item.ex
+++ lib/food_order_web/live/main/items/item/item.ex
@@ -1,4 +1,22 @@
 defmodule FoodOrderWeb.Main.Items.Item do
   use FoodOrderWeb, :live_component
+  alias FoodOrder.Carts
   alias FoodOrder.Products
+
+  def handle_event("add", _, socket) do
+    update_cart(socket)
+
+    socket =
+      socket
+      |> put_flash(:info, "Item added to cart")
+      |> push_redirect(to: "/")
+
+    {:noreply, socket}
+  end
+
+  defp update_cart(socket) do
+    product = socket.assigns.product
+    cart_id = socket.assigns.cart_id
+    Carts.add(cart_id, product)
+  end
 end
