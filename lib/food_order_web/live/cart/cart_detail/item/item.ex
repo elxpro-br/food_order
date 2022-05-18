1657c52569efb411c642cac1a9ca8ebfcb88c0c0
Gustavo <g.92oliveira@gmail.com>
2022-05-18 10:14:01 -0300

    funcionality to add products

--- lib/food_order_web/live/cart/cart_detail/item/item.ex
+++ lib/food_order_web/live/cart/cart_detail/item/item.ex
@@ -1,3 +1,4 @@
 defmodule FoodOrderWeb.CartLive.CartDetail.Item do
   use FoodOrderWeb, :live_component
+  alias FoodOrder.Products
 end
--- lib/food_order_web/live/cart/cart_live.ex
+++ lib/food_order_web/live/cart/cart_live.ex
@@ -1,9 +1,12 @@
 defmodule FoodOrderWeb.CartLive do
   use FoodOrderWeb, :live_view
+  alias FoodOrder.Carts
   # alias __MODULE__.EmptyCart
   alias __MODULE__.CartDetail
 
   def mount(_p, _s, socket) do
+    cart = Carts.get(socket.assigns.cart_id)
+    socket = assign(socket, cart: cart)
     {:ok, socket}
   end
 end
