8d6446ac836ef79df031186427e6f5d14e866712
Gustavo <g.92oliveira@gmail.com>
2022-05-18 12:21:39 -0300

    acoes  no liveview

--- lib/food_order_web/live/cart/cart_detail/item/item.ex
+++ lib/food_order_web/live/cart/cart_detail/item/item.ex
@@ -1,4 +1,29 @@
 defmodule FoodOrderWeb.CartLive.CartDetail.Item do
   use FoodOrderWeb, :live_component
+  alias FoodOrder.Carts
   alias FoodOrder.Products
+
+  def handle_event("inc", _, socket) do
+    product_id = socket.assigns.id
+    cart_id = socket.assigns.cart_id
+    cart = Carts.inc(cart_id, product_id)
+    send(self(), {:update, cart})
+    {:noreply, socket}
+  end
+
+  def handle_event("dec", _, socket) do
+    product_id = socket.assigns.id
+    cart_id = socket.assigns.cart_id
+    cart = Carts.dec(cart_id, product_id)
+    send(self(), {:update, cart})
+    {:noreply, socket}
+  end
+
+  def handle_event("remove", _, socket) do
+    product_id = socket.assigns.id
+    cart_id = socket.assigns.cart_id
+    cart = Carts.remove(cart_id, product_id)
+    send(self(), {:update, cart})
+    {:noreply, socket}
+  end
 end
