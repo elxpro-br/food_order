00b6a615d20d500207fffe85210b2b1562795cff
Gustavo <g.92oliveira@gmail.com>
2022-05-19 09:54:51 -0300

    hook to carts

--- lib/food_order_web/live/live_sessions/cart.ex
+++ lib/food_order_web/live/live_sessions/cart.ex
@@ -5,10 +5,12 @@ defmodule LiveSessions.Cart do
   # alias FoodOrderWeb.Router.Helpers, as: Routes
 
   def on_mount(:default, _params, session, socket) do
+    cart_id = get_connect_params(socket)["cart_id"]
+
     socket =
       socket
       |> assign_user(session["user_token"])
-      |> create_cart()
+      |> create_cart(cart_id)
 
     {:cont, socket}
   end
@@ -19,16 +21,24 @@ defmodule LiveSessions.Cart do
     assign_new(socket, :current_user, fn -> Accounts.get_user_by_session_token(user_token) end)
   end
 
-  defp create_cart(socket) do
+  defp create_cart(socket, cart_id) do
     current_user = socket.assigns.current_user
+    cart_id = create_cart_id(current_user, cart_id)
 
-    if current_user != nil do
-      Carts.create(current_user.id)
+    socket
+    |> assign(cart_id: cart_id)
+    |> push_event("create-cart-session-id", %{"cartId" => cart_id})
+  end
 
-      socket
-      |> assign(cart_id: current_user.id)
-    else
-      socket
-    end
+  defp create_cart_id(nil, cart_id) do
+    cart_id = (cart_id == nil && Ecto.UUID.generate()) || cart_id
+    Carts.create(cart_id)
+    cart_id
+  end
+
+  defp create_cart_id(current_user, _cart_id) do
+    cart_id = current_user.id
+    Carts.create(cart_id)
+    cart_id
   end
 end
