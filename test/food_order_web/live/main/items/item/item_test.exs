66cb6e279222da1a0042e86f8d1458c832fa18ad
Gustavo <g.92oliveira@gmail.com>
2022-05-20 08:10:25 -0300

    test behavior when click to add an item

--- test/food_order_web/live/main/items/item/item_test.exs
+++ test/food_order_web/live/main/items/item/item_test.exs
@@ -24,4 +24,20 @@ defmodule FoodOrderWeb.Main.Items.ItemTest do
 
     assert has_element?(view, "[data-role=product-add][data-id=item-#{product.id}]")
   end
+
+  test "add a new item on cart", %{conn: conn} do
+    product = insert(:product)
+    route = Routes.main_path(conn, :index)
+    {:ok, view, _html} = live(conn, route)
+    product_element = "[data-role=product-add][data-id=item-#{product.id}]"
+    assert has_element?(view, product_element)
+
+    {:ok, _view, html} =
+      view
+      |> element(product_element)
+      |> render_click()
+      |> follow_redirect(conn, route)
+
+    assert html =~ "Item added to cart"
+  end
 end
