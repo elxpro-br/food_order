70babc5d3906d4a0910f1f322e3646c0ba0e7819
Gustavo <g.92oliveira@gmail.com>
2022-05-27 07:51:13 -0300

    create order

--- lib/food_order_web/live/cart/cart_detail/confirm_order/confirm_order.ex
+++ lib/food_order_web/live/cart/cart_detail/confirm_order/confirm_order.ex
@@ -1,3 +1,24 @@
 defmodule FoodOrderWeb.CartLive.CartDetail.ConfirmOrder do
   use FoodOrderWeb, :live_component
+  alias FoodOrder.Orders
+
+  def handle_event("create_order", params, socket) do
+    case Orders.create_order_by_cart(params) do
+      {:ok, _order} ->
+        socket =
+          socket
+          |> put_flash(:info, "Order Created with Success")
+          |> push_redirect(to: Routes.customer_order_path(socket, :index))
+
+        {:noreply, socket}
+
+      {:error, _changeset} ->
+        socket =
+          socket
+          |> put_flash(:error, "Something went wrong please verify your order")
+          |> push_redirect(to: Routes.cart_path(socket, :index))
+
+        {:noreply, socket}
+    end
+  end
 end
