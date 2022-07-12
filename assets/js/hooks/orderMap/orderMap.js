af266b6cd4b332c7e355dafeb05e68724c0a5c52
Gustavo Oliveira <g.92oliveira@gmail.com>
2022-07-12 19:12:58 -0300

    show orders in the map

--- assets/js/hooks/orderMap/orderMap.js
+++ assets/js/hooks/orderMap/orderMap.js
@@ -4,8 +4,13 @@ import OrderLeaflet from "./orderLeaflet";
 const OrderMap = {
   mounted() {
 
-    this.map = new OrderLeaflet(this.el, [ -22.74470633413637, -47.34198859499476], event => {
-      
+    this.map = new OrderLeaflet(this.el, [-22.74470633413637, -47.34198859499476], event => {
+
+    })
+
+    const orders = JSON.parse(this.el.dataset.orders);
+    orders.forEach(order => {
+      this.map.addMarker(order)
     })
   }
 }
