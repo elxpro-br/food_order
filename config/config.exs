d9222666a57d1e1df0e40233819d0d9570961cdc
Gustavo Oliveira <g.92oliveira@gmail.com>
2022-07-12 18:09:56 -0300

    create service to get lat and lng by address

--- config/config.exs
+++ config/config.exs
@@ -47,6 +47,8 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :tesla, adapter: Tesla.Adapter.Hackney
+
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
